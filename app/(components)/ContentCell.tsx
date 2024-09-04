469c8f55fad11ca22354298e69a674455d301368
Danish Nasarudin <86049685+DanishNasarudin@users.noreply.github.com>
2024-09-04 14:30:39 +0800

    v0.0.3

--- app/(components)/ContentCell.tsx
+++ app/(components)/ContentCell.tsx
@@ -11,8 +11,8 @@ type Props = {
 const ContentCell = ({ title, desc, thumbnail, details, link }: Props) => {
   return (
     <TransitionLink href={link} className="w-full">
-      <div className="relative bg-white/10 w-full aspect-video rounded-md overflow-hidden border-[1px] border-white/15 flex flex-col justify-end p-4 group cursor-pointer">
-        <div className=" bg-gradient-to-t from-transparent from-10%  w-[70%] aspect-[3/2] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] rounded-md overflow-hidden mobilehover:group-hover:translate-y-[-52%] transition-all duration-500">
+      <div className="relative bg-white/10 w-full aspect-[3/2] md:aspect-video rounded-md overflow-hidden border-[1px] border-white/15 flex flex-col justify-end p-4 group cursor-pointer">
+        <div className=" bg-gradient-to-t from-transparent from-10%  w-[70%] aspect-[3/2] absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-60%] md:translate-y-[-50%] rounded-md overflow-hidden mobilehover:group-hover:translate-y-[-62%] md:mobilehover:group-hover:translate-y-[-52%] transition-all duration-500">
           <img
             src={thumbnail}
             alt={link}
@@ -22,7 +22,7 @@ const ContentCell = ({ title, desc, thumbnail, details, link }: Props) => {
 
         <div className="flex justify-between text-xs z-[2]">
           <p>{title}</p>
-          <p className="text-zinc-400">{desc}</p>
+          <p className="text-zinc-400 text-right">{desc}</p>
         </div>
       </div>
     </TransitionLink>
